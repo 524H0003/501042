520b2f4a5146cccb730d241c9425894f1b79a98b
takahashinguyen <vietuk.nguyen.git@icloud.com>
2024-09-23 10:31:13 +0700

    23/09/2024

---
+++ bai01_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<pii>
+#define vll vector<pll>
+
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)
+
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%.1f\n", (float)(a + b + c) / 3);
+
+	return 0;
+}
---
+++ bai01_2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<pii>
+#define vll vector<pll>
+
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i)
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)
+
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	if (a > b)
+	{
+		a ^= b;
+		b ^= a;
+		a ^= b;
+	}
+
+	printf("%d %d", a, b);
+
+	return 0;
+}
